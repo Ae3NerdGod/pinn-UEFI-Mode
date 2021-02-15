035ad0eaa2f2fbb6b3819cc961ac60487922b7de
Ae3NerdGod <Ae3NerdGod@gmail.com>
2021-02-15 09:03:01 -0500

    WHY THO
    i 4got thangs

--- os/uefi_extended_freespace/partition_setup.sh
+++ os/uefi_extended_freespace/partition_setup.sh
@@ -12,9 +12,9 @@ settingspart=`blkid | grep "LABEL=\"SETTINGS\"" |awk '{print $1}' | sed 's/.$//'
 echo drive is $drive part is $part partnum is $partnum settingspart is $settingspart
 
 umount /settings
-sleep 1
+sleep 3
 parted -s $drive rm $partnum
-sleep 1
+sleep 3
 mount $settingspart /settings
 
 
