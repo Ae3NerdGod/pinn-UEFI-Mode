ffe17f9fbb7dcd817b58ac5e3f6e3213e80625b7
Ae3NerdGod <Ae3NerdGod@gmail.com>
2021-02-17 05:14:56 -0500

    v3 lets see
    stable base?

--- os/uefi_extended_freespace/partition_setup.sh
+++ os/uefi_extended_freespace/partition_setup.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
+#supports_backup in PINN
+if [ -z "$restore" ]; then
 
-#drive=`echo $part1 | sed 's/.$//'`
+echo "please re-run partition setup for this \"os\" -- extended freespace is two-step process " >>/tmp/debug
+exit 0
+fi
 
-
+partprobe
 part=`blkid | grep "LABEL=\"DELTHIS\"" | awk '{print $1}' | sed 's/.$//'`
 drive=`echo -n $part |  sed 's/.$//'`
 
-partnum=` echo -n $part | tail -c 1 `
+partnum=`echo -n $part | tail -c 1`
 
 settingspart=`blkid | grep "LABEL=\"SETTINGS\"" |awk '{print $1}' | sed 's/.$//' `
 echo drive is $drive part is $part partnum is $partnum settingspart is $settingspart
 
-umount /settings
-sleep 3
-parted -s $drive rm $partnum
-sleep 3
-mount $settingspart /settings
+sync
 
-
-
-
+umount /settings 
+parted -s $drive rm $partnum 
+mount $settingspart /settings 
 
 
 exit 0
+
+
